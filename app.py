0cf08b59b747d2dd06a1da177006a3d3d0748f81
Atharva Anand Tembhurnikar <tembhurnikaraa@rknec.edu>
2025-06-24 16:33:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "https://your-deployed-frontend.com"], 
+CORS(app, resources={r"/*": {"origins": "*", 
                             "methods": ["GET", "POST", "OPTIONS"], 
                             "allow_headers": ["Content-Type"]}})
 
